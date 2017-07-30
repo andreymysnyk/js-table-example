3d2a4d112f8f3306bd2689f200499072fc5c7c6e
Vika Petrenko <vika-miranet@yandex.ru>
2017-07-31 01:00:08 +0300

    sort list

--- table-app/src/app/task-list/task-list.component.ts
+++ table-app/src/app/task-list/task-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Task } from '../task';
 import { TasksService } from '../tasks.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-task-list',
@@ -10,7 +11,12 @@ import { TasksService } from '../tasks.service';
 })
 export class TaskListComponent implements OnInit {
 
+  TIME_FORMAT_TIME = "HH:mm";
+  TIME_FORMAT_DATETIME = "YYYY-MM-DD HH:mm";
+  TIME_FORMAT_DATE = "YYYY-MM-DD";
+
   tasks: Task[];
+  map = new Map();
 
   constructor(private tasksService: TasksService) {
   }
@@ -20,11 +26,39 @@ export class TaskListComponent implements OnInit {
   }
 
   getTasks(): void {
-    this.tasksService.getTasks().then(tasks => this.tasks = tasks);
+    this.tasksService.getTasks().then(tasks => {
+      this.tasks = tasks;
+      this._setMap();
+    });
   }
 
   onSave(task: Task) {
-    //TODO
-    console.log(task)
+    this.tasks.push(task);
+  }
+
+  static _sortEvents(a,b) {
+    return new Date(a.date).getTime() - new Date(b.date).getTime()
+  }
+
+  _setMap() {
+    // sort tasks
+    this.tasks = this.tasks.sort(TaskListComponent._sortEvents);
+
+    this.tasks.forEach(task => {
+
+      // get date key
+      let dateKey = moment(task.date, this.TIME_FORMAT_DATETIME).format(this.TIME_FORMAT_DATE);
+
+      // get tasks list for date, append
+      let currentTasks = this.map.get(dateKey);
+
+      currentTasks = currentTasks || [];
+      currentTasks.push(task);
+
+      // set again
+      this.map.set(dateKey, currentTasks);
+    });
+
+    console.log(this.map);
   }
 }
